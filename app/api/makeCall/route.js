a9240365563344e4486e116a907881935e0d0a9e
Emir  Karabeg <emirkarabeg@berkeley.edu>
2024-04-27 10:41:39 -0700

    Added comment messages

--- app/api/makeCall/route.js
+++ app/api/makeCall/route.js
@@ -19,3 +19,25 @@ export async function POST(req, res) {
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
+
+// Call agent
+// Tell agent number
+// Tell agent what you want to do
+
+// DB STORES CALL SID
+
+// Agent calls airline
+// Drops your call
+// Agent hears options
+// Transcribes options and formats
+// Query GPT with options (text + goal)
+
+// GPT
+
+// Get back digit response
+// Send digit response to airline
+// Wait for human
+// Human responds
+// Call user
+
+// Build simulation phone number (United) options 1 and 2 -> put on hold and play music for 1 minute -> respond with human talking (pick up after 3 seconds)
