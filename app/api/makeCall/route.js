c87fa71ecde53d6949df550900d5ff93bc9d1fce
Artem Tkachuk <artemtkachuk@yahoo.com>
2024-04-27 11:36:44 -0700

    Initialize edgedb

--- app/api/makeCall/route.js
+++ app/api/makeCall/route.js
@@ -9,11 +9,15 @@ export async function POST(req, res) {
     
     try {
         const call = await client.calls.create({
-        url: 'http://demo.twilio.com/docs/voice.xml',
-        to: '+17144255363',
-        from: '+18773212978',
+            url: 'http://demo.twilio.com/docs/voice.xml',
+            to: '+17144255363',
+            from: '+18773212978',
         });
-        return NextResponse.json({ callSid: call.sid }, { status: 200 })
+        return NextResponse.json({
+            callSid: call.sid 
+        }, {
+            status: 200 
+        })
     } catch (error) {
         console.log(error);
         return NextResponse.json({ error: error.message }, { status: 500 })
